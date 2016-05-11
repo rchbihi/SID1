9c15dba7e45a1d7c95b90719953b305fba5be289
sihamlamssaoui <sihamlamssaoui@gmail.com>
2016-05-11 16:01:24 +0100

    Update commoninit.php

--- Model/commoninit.php
+++ Model/commoninit.php
@@ -9,4 +9,20 @@ session_start();
  *inclure le fichier autoload.php qui recupere les classes
  */
 require_once "autoload.php";
+
+require 'BD/compte.php';
+require 'BD/action.php';
+require 'BD/actualite.php';
+require 'BD/categorie.php';
+require 'BD/competence.php';
+require 'BD/contenir.php';
+require 'BD/document.php';
+require 'BD/experience.php';
+require 'BD/formation.php';
+require 'BD/possede.php';
+require 'BD/post_fil_actualite.php';
+require 'BD/post_visibilite.php';
+require 'BD/travaux.php';
+require 'BD/type_utilisateur.php';
+require 'BD/visibilite_doc.php';
 ?>
