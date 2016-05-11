3238dfab38fc3537fab0f91b38e988e9e51d583e
sihamlamssaoui <sihamlamssaoui@gmail.com>
2016-05-11 16:00:40 +0100

    Update get.php

--- Model/get.php
+++ Model/get.php
@@ -3,21 +3,6 @@
  * @author LAMSSAOUI Siham && TADILI Ali
  */
 require 'commoninit.php';
-require 'BD/compte.php';
-require 'BD/action.php';
-require 'BD/actualite.php';
-require 'BD/categorie.php';
-require 'BD/competence.php';
-require 'BD/contenir.php';
-require 'BD/document.php';
-require 'BD/experience.php';
-require 'BD/formation.php';
-require 'BD/possede.php';
-require 'BD/post_fil_actualite.php';
-require 'BD/post_visibilite.php';
-require 'BD/travaux.php';
-require 'BD/type_utilisateur.php';
-require 'BD/visibilite_doc.php';
 
 
 
